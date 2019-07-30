b217f310979d94062780e24e1cfbab205bb7cff8
matt712 <mzgadd@gmail.com>
2019-07-30 16:04:21 +0100

    players can now win games

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -5,22 +5,26 @@ class Game(matches: List[Match]) {
   def playTournament(): Unit ={
     for(x <- this.matches){
       x.setStatus(MatchStatusEnum.InProgress)
-      
+      println(x.toString)
+      println("Type in the winners nickname")
+      var winner = playMatch(x, scala.io.StdIn.readLine()).getWinnerName
+      println("Next match")
     }
   }
-  def playMatch(thisMatch: Match, winner: String): String = {
-    println(thisMatch.toString)
-    println("Type in the winners nickname")
-    if (winner == thisMatch.getPlayerOne.nickname) {
+  def playMatch(thisMatch: Match, winner: String): Match = winner match {
+    case thisMatch.getPlayerOne.nickname => {
+      thisMatch.getPlayerOne.giveWin()
       thisMatch.setWinnerNickname(thisMatch.getPlayerOne.nickname)
-      "success"
     }
-    else if (winner == thisMatch.getPlayerTwo.nickname) {
+    case thisMatch.getPlayerTwo.nickname  => {
+      thisMatch.getPlayerTwo.giveWin()
       thisMatch.setWinnerNickname(thisMatch.getPlayerTwo.nickname)
-      "success"
     }
-    else {
-      "retry"
+    case _ => {
+      println("typing error")
+      println(thisMatch.toString)
+      println("Type in the winners nickname")
+      playMatch(thisMatch, scala.io.StdIn.readLine())
     }
   }
 }
--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -2,6 +2,9 @@ class Player (val firstname: String , val surname: String ,val nickname: String 
 
 
   val uniquieID = {this.firstname + this.surname + this.nickname}.reverse.replace(" ","")
+  def giveWin() : Unit = {
+    this.allTimeWins +=1
+  }
 
   def displayInfo (): Unit = {
     println(s"Firstname:${this.firstname} \nSurname:${this.surname} \nNickname:${this.nickname} \nUniquie Code:${this.uniquieID} \nWins:${this.allTimeWins} \nLoss:${this.allTimeLoses} ")
