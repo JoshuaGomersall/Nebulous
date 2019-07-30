f8b772868a1f748ebdc7d516f2d5528a89a0140e
JoshuaGomersall <GingerPete12>
2019-07-30 22:35:35 +0100

    To String Test Added

--- src/test/scala/PlayerTest.scala
+++ src/test/scala/PlayerTest.scala
@@ -9,6 +9,12 @@ class PlayerTest extends FlatSpec with Matchers {
     assert("john" == playerOne.firstname)
   }
 
+  it should "display the current in the correct format" in {
+    val playerOne = new Player("john", "smith", "bigbadjohn", 0, 0)
+    println(playerOne)
+    assert("Player(john,smith,bigbadjohn,0,0)" == playerOne.toString)
+  }
+
   it should "make a new user and have values it was given" in {
     val playerOne = new Player("john", "smith", "bigbadjohn", 0, 0)
     assert("john" == playerOne.firstname)
@@ -18,7 +24,6 @@ class PlayerTest extends FlatSpec with Matchers {
 
   it should "make a new user and display its info" in {
     val playerOne = new Player("john", "smith", "bigbadjohn", 0, 0)
-    println(playerOne.displayInfo())
     assert("Firstname:john \nSurname:smith \nNickname:bigbadjohn \nUniquie Code:nhojdabgibhtimsnhoj \nWins:0 \nLoss:0" == playerOne.displayInfo())
   }
 
