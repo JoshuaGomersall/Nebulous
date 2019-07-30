37c007346117f14a4a83bac44897140edb3decf5
JoshuaGomersall <GingerPete12>
2019-07-30 13:01:12 +0100

    More Parameters Added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    val greg = new Player("Greg","Simons","Greggi Boi")
-
-
-    greg.displayInfo()
-
-
-}
+  }
 }
 
 
--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -1,12 +1,11 @@
-class Player (val firstname: String , val surname: String ,val nickname: String , uniquieID : String = "placeHolder"    ,totalWins :Int = 0) {
+class Player (val firstname: String , val surname: String ,val nickname: String ,allTimeWins : Int = 0,allTimeLoses :Int = 0) {
 
 
-
-
+  val uniquieID = {this.firstname + this.surname + this.nickname}.reverse.replace(" ","")
 
 
   def displayInfo (): Unit = {
-    println(s"Firstname:${this.firstname} \nSurname:${this.surname} \nNickname:${this.nickname}         Total Wins:${this.totalWins}       ")
+    println(s"Firstname:${this.firstname} \nSurname:${this.surname} \nNickname:${this.nickname} \nUniquie Code:${this.uniquieID} \nWins:${this.allTimeWins} \nLoss:${this.allTimeLoses} ")
   }
 
 }
