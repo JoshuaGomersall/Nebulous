85dcfef4ad9982b3ed2822ecef30351808c1b157
matt712 <mzgadd@gmail.com>
2019-07-31 14:25:36 +0100

    Game less broken now, need to display player with most tournament wins

--- src/main/scala/classes/Game.scala
+++ src/main/scala/classes/Game.scala
@@ -4,37 +4,40 @@ import utils.MatchStatusEnum
 
 class Game(matches: List[Match], players: List[Player]) {
 
-  def playTournament(): Unit ={
-    for(x <- players){
+  def playTournament(): Unit = {
+    for (x <- players) {
       x.newTournamentStart()
     }
-    for(x <- this.matches){
+    for (x <- this.matches) {
       x.setStatus(MatchStatusEnum.InProgress)
       println(x.toString)
       println("Type in the winners nickname")
-      playMatch(x, x.getPlayerOne.getNickname, x.getPlayerTwo.getNickname,  scala.io.StdIn.readLine())
+      playMatch(x, x.getPlayerOne.nickname, x.getPlayerTwo.nickname, scala.io.StdIn.readLine())
       println("Next match")
     }
-    this.players.sortWith(_.getTournamentWins() > _.getTournamentWins())
-    println(s"The winner is ${this.players(0)}")
+    println(s"The winner is ${players(0)}") //This line is broken, need to find teh winnar
   }
 
-  def playMatch(thisMatch: Match,nickname1: String, nickname2: String, winner: String): Match = winner match {
-   case nickname1 => {
-      thisMatch.getPlayerOne.giveWin()
-     thisMatch.getPlayerTwo.giveLose()
-      thisMatch.setWinnerNickname(thisMatch.getPlayerOne.nickname)
-    }
-    case nickname2  => {
-      thisMatch.getPlayerTwo.giveWin()
-      thisMatch.getPlayerOne.giveLose()
+  def playMatch(thisMatch: Match, nickname1: String, nickname2: String, winner: String): Match = {
+    println(nickname1)
+    println(nickname2)
+    println(winner)
+    winner match {
+      case thisMatch.playerOne.nickname =>
+        println("Glip")
+        thisMatch.getPlayerOne.giveWin()
+        thisMatch.getPlayerTwo.giveLose()
+        thisMatch.setWinnerNickname(thisMatch.getPlayerOne.nickname)
+      case thisMatch.playerTwo.nickname =>
+        println("Glop")
+        thisMatch.getPlayerTwo.giveWin()
+        thisMatch.getPlayerOne.giveLose()
         thisMatch.setWinnerNickname(thisMatch.getPlayerTwo.nickname)
-      }
-    case _ => {
-      println("typing error")
-      println(thisMatch.toString)
-      println("Type in the winners nickname")
-      playMatch(thisMatch, nickname1, nickname2, scala.io.StdIn.readLine())
+      case _ =>
+        println("typing error")
+        println(thisMatch.toString)
+        println("Type in the winners nickname")
+        playMatch(thisMatch, nickname1, nickname2, scala.io.StdIn.readLine())
     }
   }
 }
--- src/main/scala/classes/Match.scala
+++ src/main/scala/classes/Match.scala
@@ -3,7 +3,7 @@ package classes
 import utils.MatchStatusEnum
 
 
-class Match(playerOne: Player, playerTwo: Player, status: MatchStatusEnum.Value = MatchStatusEnum.WaitingForStart, winnerName: String = "") {
+class Match(val playerOne: Player, val playerTwo: Player, status: MatchStatusEnum.Value = MatchStatusEnum.WaitingForStart, winnerName: String = "") {
 
   override def toString: String = {
     status match {
