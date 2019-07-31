ce1299977a11a320016110dc2bc048d5f4d51747
matt712 <mzgadd@gmail.com>
2019-07-31 10:49:16 +0100

    Tournament method finished

--- src/main/scala/classes/Game.scala
+++ src/main/scala/classes/Game.scala
@@ -5,6 +5,9 @@ import utils.MatchStatusEnum
 class Game(matches: List[Match], players: List[Player]) {
 
   def playTournament(): Unit ={
+    for(x <- players){
+      x.newTournamentStart()
+    }
     for(x <- this.matches){
       x.setStatus(MatchStatusEnum.InProgress)
       println(x.toString)
@@ -12,19 +15,19 @@ class Game(matches: List[Match], players: List[Player]) {
       var winner = playMatch(x, scala.io.StdIn.readLine()).getWinnerName
       println("Next match")
     }
-    //this.players.sortWith(_.getWins() > _.getWins())
+    this.players.sortWith(_.getTournamentWins() > _.getTournamentWins())
     println(s"The winner is ${this.players(0)}")
   }
 
   def playMatch(thisMatch: Match, winner: String): Match = winner match {
-//    case thisMatch.getPlayerOne.nickname => {
-//      //thisMatch.getPlayerOne.giveWin()
-//      thisMatch.setWinnerNickname(thisMatch.getPlayerOne.nickname)
-//    }
-//    case thisMatch.getPlayerTwo.nickname  => {
-//      //thisMatch.getPlayerTwo.giveWin()
-//      thisMatch.setWinnerNickname(thisMatch.getPlayerTwo.nickname)
-//    }
+   case thisMatch.getPlayerOne.nickname => {
+      //thisMatch.getPlayerOne.giveWin()
+      thisMatch.setWinnerNickname(thisMatch.getPlayerOne.nickname)
+    }
+    case thisMatch.getPlayerTwo.nickname  => {
+      thisMatch.getPlayerTwo.giveWin()
+        thisMatch.setWinnerNickname(thisMatch.getPlayerTwo.nickname)
+      }
     case _ => {
       println("typing error")
       println(thisMatch.toString)
