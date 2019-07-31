2b2f6a54e7979f70898f69cfa7568bdd211f7533
VLukovski <valentinlukovski1@gmail.com>
2019-07-31 11:48:43 +0100

    tied everything up in menu

--- src/main/scala/AppMain.scala
+++ src/main/scala/AppMain.scala
@@ -4,13 +4,6 @@ import utils.playerUtils.PlayerReadWrite.{initialisePlayerFile, readPlayerList}
 object AppMain {
 
   def main(args: Array[String]): Unit = {
-    val tempPlayerList = readPlayerList("file")
-    if(tempPlayerList.isEmpty){
-      initialisePlayerFile
-      val playerList = readPlayerList("file")
-    } else {
-      val playerList = tempPlayerList
-    }
-    Menu.startMenu(tempPlayerList)
+    Menu.startMenu
   }
 }
