4ce0bdc00a59946a1f059459d053776eba8fe264
JoshuaGomersall <GingerPete12>
2019-07-31 11:33:44 +0100

    Menu Tests Added

--- src/test/scala/objects/MenuTest.scala
+++ src/test/scala/objects/MenuTest.scala
@@ -1,9 +1,15 @@
 package objects
 
 import org.scalatest._
+import Menu._
+import classes.Player
 
 
 class MenuTest extends FlatSpec with Matchers {
 
+  val john = new Player("john" ,"smith" ,"bigbdjohn")
 
+  it should "load the menu for user input" in {
+    assertCompiles("Menu.startMenu(List(john, john))")
+  }
 }
