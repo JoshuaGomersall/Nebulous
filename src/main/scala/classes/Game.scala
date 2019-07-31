fdf3206eff688b1cae7f90ac047cc5933ad9eaf3
BetaDraconis <lukeajones11996@gmail.com>
2019-07-31 15:37:49 +0100

    Added new implementation of 'playTournament' method

--- src/main/scala/classes/Game.scala
+++ src/main/scala/classes/Game.scala
@@ -1,32 +1,36 @@
 package classes
 
 import utils.MatchStatusEnum
+import scala.collection.mutable.ListBuffer
 
 class Game(matches: List[Match], players: List[Player]) {
 
   def playTournament(): Unit = {
+    val playerList = new ListBuffer[Player]()
     players.foreach(player => player.newTournamentStart())
     matches.foreach(tempMatch => {
       tempMatch.setStatus(MatchStatusEnum.InProgress)
       println(tempMatch.toString)
       println("Type in the winners nickname")
-      playMatch(tempMatch, tempMatch.getPlayerOne.nickname, tempMatch.getPlayerTwo.nickname, scala.io.StdIn.readLine())
+      playerList += playMatch(tempMatch, tempMatch.getPlayerOne.nickname, tempMatch.getPlayerTwo.nickname, scala.io.StdIn.readLine())
       println("Next match")
     })
-    println(s"The winner is ${players(0)}") //This line is broken, need to find teh winnar
+    println(s"The winner is ${playerList.toList.maxBy(player => player.tournamentWins).nickname}") //This line is broken, need to find teh winnar
   }
 
 
-  def playMatch(thisMatch: Match, nickname1: String, nickname2: String, winner: String): Match = {
+  def playMatch(thisMatch: Match, nickname1: String, nickname2: String, winner: String): Player = {
     winner match {
       case thisMatch.playerOne.nickname =>
         thisMatch.getPlayerOne.giveWin()
         thisMatch.getPlayerTwo.giveLose()
         thisMatch.setWinnerNickname(thisMatch.getPlayerOne.nickname)
+        thisMatch.playerOne
       case thisMatch.playerTwo.nickname =>
         thisMatch.getPlayerTwo.giveWin()
         thisMatch.getPlayerOne.giveLose()
         thisMatch.setWinnerNickname(thisMatch.getPlayerTwo.nickname)
+        thisMatch.playerTwo
       case _ =>
         println("typing error")
         println(thisMatch.toString)
