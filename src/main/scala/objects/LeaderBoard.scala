f8211c20cfe34762bc22cfb864764173169dfe41
Joseph Simmons <jwmsimmons@gmail.com>
2019-07-31 11:55:39 +0100

    back to Main Menu Added

--- src/main/scala/objects/LeaderBoard.scala
+++ src/main/scala/objects/LeaderBoard.scala
@@ -10,22 +10,28 @@ object LeaderBoard {
       "Please choose what you would like to do: \n" +
       "1 - View Best Ratio\n" +
       "2 - View Most Wins\n" +
-      "3 - View Most Loses\n")
+      "3 - View Most Loses\n" +
+      "4 - Back to main Menu\n")
     scala.io.StdIn.readLine() match {
       case "1" =>
         println("Still Calculating")
         println("\n")
+        leaderBoardMenu(list)
       case "2" =>
         mostWins(list)
         println("\n")
+        leaderBoardMenu(list)
       case "3" =>
         mostLoses(list)
         println("\n")
+        leaderBoardMenu(list)
+      case "4" =>
+        println("You exited the LeaderBoard\n")
       case _ =>
         println("Invalid input, try again")
         leaderBoardMenu(list)
     }
-    leaderBoardMenu(list)
+
   }
   def printSimpleResult(list: List[Player]): Unit = {
     mostWins(list)
